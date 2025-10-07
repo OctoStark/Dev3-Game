cad6a681066aa9365e5a4a31ff9d11ba083ad39b
OctoStark <robantsch32@gmail.com>
2025-10-07 04:35:51 -0400

    spike trap is done and dart trap should be good too

---
+++ Dev3Game/Assets/Scripts/Traps.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+    enum trapType {Spike, Dart}
+    [SerializeField] trapType type;
+    [SerializeField] Transform[] holePos;
+    [SerializeField] GameObject dmgObj;
+    //[SerializeField] Rigidbody rb;
+
+    [SerializeField] float shootRate;
+    [SerializeField] int waitTime;
+    [SerializeField] int trapDuration;
+
+    float dartTimer;
+    float spikeTimer;
+    float shootTimer;
+    bool playerInTrigger;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spikeTimer += Time.deltaTime;
+        shootTimer += Time.deltaTime;
+
+        if (type == trapType.Spike)
+        {
+            if (spikeTimer >= waitTime)
+            {
+                dmgObj.SetActive(true);
+                //spikeTimer = 0;
+                if (spikeTimer >= trapDuration)
+                {
+                dmgObj.SetActive(false);
+                spikeTimer = 0;
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            if (type == trapType.Dart)
+            {
+                dartTimer = 0;
+                dartTimer += Time.deltaTime;
+                if (dartTimer <= trapDuration)
+                {
+                    if (shootTimer >= shootRate)
+                    {
+                        shoot();
+                    }
+                }
+            }
+        }
+    }
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(dmgObj, holePos[holePos.Length].position, transform.rotation);
+    }
+}
