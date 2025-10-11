b8a2358e6e730e25959fae617af8c883bc7a5c1b
Mee6hy <Dpalmer1996@gmail.com>
2025-10-10 22:24:52 -0400

    .

---
+++ Dev3Game/Assets/Scripts/TutorialPopUp.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class TutorialPopUp : MonoBehaviour
+{
+    public static TutorialPopUp instance;
+
+    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] TextMeshProUGUI tutorialText;
+    [SerializeField] float fadeDuration = 0.5f;
+
+    private Coroutine fadeRoutine;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        canvasGroup.alpha = 0;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void ShowMessage(string message, float duration = 3f)
+    {
+        if(fadeRoutine != null)
+            StopCoroutine(fadeRoutine);
+
+        tutorialText.text = message;
+        gameObject.SetActive(true);
+        fadeRoutine = StartCoroutine(ShowAndHide(duration));
+    }
+
+    private IEnumerator ShowAndHide(float duration)
+    {
+        yield return FadeCanvas(1f);
+
+        yield return new WaitForSeconds(duration);
+
+        yield return FadeCanvas(0f);
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator FadeCanvas(float targetAlpha)
+    {
+        float startAlpha = canvasGroup.alpha;
+        float time = 0f;
+
+        while (time < fadeDuration) {
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, time / fadeDuration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = targetAlpha;
+    }
+}
