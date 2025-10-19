9d4952ee18b9427e735f3884fb964b54c090dbe2
Anna Rosario <musicanime32@gmail.com>
2025-10-19 17:50:07 -0400

    Revert "Simple Attack Animation"
    This reverts commit 51279af70ca46fa1cebdf43cb8cf93afcb598afd.

--- Dev3Game/Assets/Scripts/Weapon/WeaponStats.cs
+++ Dev3Game/Assets/Scripts/Weapon/WeaponStats.cs
@@ -13,9 +13,6 @@ public class WeaponStats : ScriptableObject
     public AnimationClip attackAnimation;
     public AnimationClip idleAnimation;
 
-    [Header("Animations")]
-    public AnimatorOverrideController animOverrideControl;
-
     public AnimationClip GetAttackAnimation()
     {
         return attackAnimation;
